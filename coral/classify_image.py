5f02ecd4da9824b11abf9b13dd79fd751b6de982
Jeff <jeff@cogentcoder.com>
2020-12-29 18:08:33 -0500

    Revert "Merge remote-tracking branch 'origin/main' into main"
    This reverts commit 003c7171096d62f027bb62c4e0c4b3d8c772036d.

--- coral/classify_image.py
+++ coral/classify_image.py
@@ -4,14 +4,9 @@
 
   To run this code, you must attach an Edge TPU attached to the host and
   install the Edge TPU runtime (`libedgetpu.so`) and `tflite_runtime`.
-<<<<<<< HEAD
   For device setup instructions, see g.co/coral/setup.
 
   (use `install_requirements.sh` to get these files):
-=======
-  (use `install_requirements.sh` to get these files):
-  For device setup instructions, see g.co/coral/setup.
->>>>>>> main
 
   python3 classify_image.py \
     --model models/mobilenet_v2_1.0_224_inat_bird_quant_edgetpu.tflite  \
@@ -20,10 +15,6 @@
 
    https://coral.ai/docs/accelerator/get-started/#3-run-a-model-using-the-tensorflow-lite-api
 """
-<<<<<<< HEAD
-=======
-
->>>>>>> main
 import argparse
 import time
 
